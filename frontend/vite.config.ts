9e9bc908bf8a039b37232e0f6ddcb18a2a275137
roger.barriga <roger.barriga@scribsoft.com>
2024-10-16 19:52:10 -0500

    Fixing environment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.BACKEND_SERVER': JSON.stringify(env.BACKEND_SERVER),
+      'process.env.BACKEND_PORT': JSON.stringify(env.BACKEND_PORT)
+    },
+    plugins: [react()],
+  }
 })
